6d68c9ad31a8bc981f4f52377d2c0b864e801c27
Nitesh Kumar <nk37992@gmail.com>
2025-10-16 13:25:56 +0530

    Initial commit for maven package

---
+++ entity-library/src/main/java/com/ucp/entities/entity_library/Welcome.java
@@ -0,0 +1,17 @@
+package com.ucp.entities.entity_library;
+
+public class Welcome {
+	
+	String Name;
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+	
+	
+
+}
